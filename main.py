c911215ce26b2aad8ce2d60e3ea6b44349feadb1
DawalaGH <dawalaamidudar@gmail.com>
2025-01-28 23:57:04 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 num=3
 if num>0:
-    print("number is positive")
+    print(num,"number is positive")
 
-num1=-1
-if num1<0:
-    print("number is negative")
+num1 = -1
+if num>10:
+    print(num,"number is negative")
